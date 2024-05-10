a64557bffeb0ce69e3c89292ecbe017632297d11
devaziz1 <devaziz1@gmail.com>
2024-05-10 20:20:11 +0500

    metor effect added

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
-
+import React from "react";
+import { Meteors } from "../../components/ui/meteors";
 function page() {
   return (
-    <div>page</div>
-  )
+    <div className="min-h-screen bg-black py-12 pt-36">
+      <h1 className="text-lg md:text-5xl text-center font-sans font-bold mb-8 text-white">
+        Contact Us Page
+      </h1>
+      <Meteors number={50} />
+    </div>
+  );
 }
 
-export default page
+export default page;
